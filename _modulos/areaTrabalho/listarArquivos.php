4b5d130e54471b36ce3cd42f32b9cf7e5dfac524
renatosans <renato@datacopy.com.br>
2017-01-21 20:25:19 -0200

    Inclusão de arquivo

---
+++ _modulos/areaTrabalho/listarArquivos.php
@@ -0,0 +1,13 @@
+<?php
+
+    $tempDir = "../areaTrabalho";
+
+    // Lista os arquivos do diretório temporário e retorna uma lista em formato JSON
+    $fileArray = array();
+    foreach (new DirectoryIterator($tempDir) as $fileInfo) {
+        if($fileInfo->isDir()) continue;
+        array_push($fileArray, $fileInfo->getFilename());
+    }
+    echo json_encode($fileArray);
+
+?>
