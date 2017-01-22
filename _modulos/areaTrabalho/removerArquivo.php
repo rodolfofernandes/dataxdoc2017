a22e3f4805971d6c0860b6d8318774b401343336
renatosans <renato@datacopy.com.br>
2017-01-22 15:58:56 -0200

    Alterações no projeto

---
+++ _modulos/areaTrabalho/removerArquivo.php
@@ -0,0 +1,18 @@
+<?php
+
+    $tempDir = "../areaTrabalho/_tempDir";
+
+    if( !isset($_POST['filesChecked']) ){
+        echo "Selecione os arquivos que deseja excluir";
+        exit;
+    }
+
+    foreach($_POST['filesChecked'] as $key=>$value){
+        if( !unlink($tempDir.base64_decode($value) )){
+            echo "Não foi possivel efetuar a operação...";
+            exit;
+        }
+    }
+
+    echo "Operação efetuada com sucesso!";
+?>
