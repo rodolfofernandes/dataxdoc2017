406243538bdfd8d37eca256c17cd860db8b15f24
renatosans <renato@datacopy.com.br>
2017-01-29 20:11:13 -0200

    Alterações no projeto

--- _modulos/tiposDocumentais/tabelas.sql
+++ _modulos/tiposDocumentais/tabelas.sql
@@ -13,10 +13,8 @@ CREATE TABLE IF NOT EXISTS `db_cliente`.`tbl_tipodocumental` (
   UNIQUE INDEX `idmodeloDocumento_UNIQUE` (`id` ASC))
 ENGINE = InnoDB;
 
-
-
 -- -----------------------------------------------------
--- Table `dataXDoc`.`tbl_tipoindexador`
+-- Table `db_cliente`.`tbl_tipoindexador`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `db_cliente`.`tbl_tipoindexador` ;
 
@@ -37,3 +35,29 @@ INSERT INTO `db_cliente`.`tbl_tipoindexador` (`id`, `descricao`) VALUES (2, 'TEX
 INSERT INTO `db_cliente`.`tbl_tipoindexador` (`id`, `descricao`) VALUES (3, 'DATA');
 
 COMMIT;
+
+-- -----------------------------------------------------
+-- Table `db_cliente`.`tbl_indexador`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_cliente`.`tbl_indexador` ;
+
+CREATE TABLE IF NOT EXISTS `db_cliente`.`tbl_indexador` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tipoDocumental` INT NOT NULL,
+  `nome` VARCHAR(65) NOT NULL,
+  `tipo` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `fk_indexador_tipoDocumental_idx` (`tipoDocumental` ASC),
+  INDEX `fk_indexador_tipoIndexador_idx` (`tipo` ASC),
+  CONSTRAINT `fk_indexador_tipoDocumental`
+    FOREIGN KEY (`tipoDocumental`)
+    REFERENCES `dataXDoc`.`tbl_tipodocumental` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_indexador_tipoIndexador`
+    FOREIGN KEY (`tipo`)
+    REFERENCES `dataXDoc`.`tbl_tipoindexador` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
